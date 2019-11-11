dc524181bbb47c8e853def3b77cb87e17e1355f1
nickota <xxk0ta0605@gmail.com>
2019-11-11 13:45:43 +0900

    おみくじを作ってみよう #09

--- MyOmikuji/js/main.js
+++ MyOmikuji/js/main.js
@@ -4,20 +4,16 @@
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click', () => {
-        const n = Math.floor(Math.random() * 3);
+        const results = ['大吉', '中吉', '小吉', '凶'];
+        btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-        switch (n) {
-            case 0:
-                btn.textContent = "大吉";
-                break;
-            case 1:
-                btn.textContent = "中吉";
-                break;
-            case 2:
-                btn.textContent = "小吉";
-                break;
+        const n = Math.random();
+        if (n < 0.05) {
+            btn.textContent = '大吉';
+        } else if (n < 0.2) {
+            btn.textContent = '中吉';
+        } else {
+            btn.textContent = '凶';
         }
-
-
     })
 }
